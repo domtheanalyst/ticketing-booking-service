7db425de50ff38edf8c1b0f70aa09bd21df41db0
igwebuikedominic@gmail.com <igwebuikedominic@gmail.com>
2026-06-09 21:33:09 +0100

    feat: created booking service and injected it into booking controller

--- bookingservice/src/main/java/com/drlabs/bookingservice/controller/BookingController.java
+++ bookingservice/src/main/java/com/drlabs/bookingservice/controller/BookingController.java
@@ -1,5 +1,7 @@
 package com.drlabs.bookingservice.controller;
 
+import com.drlabs.bookingservice.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/")
 public class BookingController {
+
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
 
     @PostMapping("/bookings")
     public BookingResponse createBooking(@RequestBody BookingRequest request) {
